df22cecf38607a0bc9bae4f0a70f8070e5df4430
pepdrin55 <pepdrin55@gmail.com>
2022-10-03 12:55:44 -0300

    position class

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import boardgame.Position;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
+
+
+
+    }
+
+}
